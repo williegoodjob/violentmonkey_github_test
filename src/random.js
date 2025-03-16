dc60b5b47d25aacc70d637078a268631276b2f45
willie <willie@goodjob.idv.tw>
2025-03-16 15:23:03 +0800

    重構隨機數模組，增加參數類型檢查並確保 ModuleTest 物件存在

--- src/random.js
+++ src/random.js
@@ -1,6 +1,17 @@
-window.ModuleTest.random = function () {
-    function random(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-    return random;
-};
+(function() {
+    'use strict';
+
+    // 確保 ModuleTest 物件存在
+    window.ModuleTest = window.ModuleTest || {};
+
+    // 定義 random 模組
+    window.ModuleTest.random = function() {
+        function random(min, max) {
+            if (typeof min !== 'number' || typeof max !== 'number') {
+                throw new Error('參數必須是數字');
+            }
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+        return random;
+    };
+})();
