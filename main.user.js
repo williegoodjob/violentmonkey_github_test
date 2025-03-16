9c281b9977e14f8333660e2aa462b140392f8e79
willie <willie@goodjob.idv.tw>
2025-03-16 15:21:22 +0800

    更新腳本匹配模式及修正隨機數模組的引用路徑

--- main.user.js
+++ main.user.js
@@ -4,10 +4,10 @@
 // @version      0.1
 // @description  測試瀏覽器對模組化功能的處理
 // @author       Your name
-// @match        https://*.example.com/*
+// @match        *://*/*
 // @grant        none
 // @require      https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js
-// @require      file://D:/DevProgram/Project/w3school練習掛機腳本/violentmonkey_github_test/src/random.js
+// @require      https://cdn.jsdelivr.net/gh/williegoodjob/violentmonkey_github_test@main/src/random.js
 // ==/UserScript==
 
 // 主程序
